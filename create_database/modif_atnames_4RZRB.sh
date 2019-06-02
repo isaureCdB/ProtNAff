63f029ab93b7ac8ca5cb849c3cca9843fd506f79
Isaure Chauvot de Beauchene <isaure@debeauchene.fr>
2019-06-02 23:29:41 +0200

    add camemberts from newton

---
+++ create_database/modif_atnames_4RZRB.sh
@@ -0,0 +1,12 @@
+a=cleanPDB/4RZRB-1.pdb
+sed -i "s/OAS/O3'/"  $a
+sed -i "s/CAT/C3'/"  $a
+sed -i "s/CAU/C4'/"  $a
+sed -i "s/CAV/C5'/"  $a
+sed -i "s/OAB/O5'/"  $a
+sed -i "s/PAD/P  /"  $a
+sed -i "s/CAR/C2'/"  $a
+sed -i "s/CAQ/C1'/"  $a
+sed -i "s/OAP/O4'/"  $a
+sed -i "s/OAA/OP2/"  $a
+sed -i "s/OAC/OP1/"  $a
