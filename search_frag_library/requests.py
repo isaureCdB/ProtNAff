e177fae3e92f3d149694e8efc8dd0a3cd8fe3865
Isaure Chauvot de Beauchene <isaure@debeauchene.fr>
2019-06-02 23:29:41 +0200

    add camemberts from newton

---
+++ search_frag_library/requests.py
@@ -0,0 +1,28 @@
+
+def is_ss(result):
+    ss = "".join([r[0] for r in result]).replace("T", "S")
+    if ss == "SSS":
+        return True
+    return False
+
+def is_ds(result):
+    ss = "".join([r[0] for r in result]).replace("T", "S")
+    if ss == "DDD":
+        return True
+    return False
+
+def get_mid(result):
+    if result[1] is not None:
+        return True
+    return False
+
+def get_smaller(result, cutoff):
+    if result[1] is None:
+        return False
+    if result[1] < cutoff:
+        return True
+    return False
+
+import functools
+def set_contact_parts(cutoff):
+    return functools.partial(get_smaller, cutoff = cutoff)
