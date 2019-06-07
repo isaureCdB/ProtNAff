90bc0a9f7a852864fab0870ad4378834344895b0
Isaure Chauvot de Beauchene <isaure@debeauchene.fr>
2019-06-07 14:56:37 +0200

    allow clustering of DNA and RNA in parallele

--- create_frag_library/mutate-AC-libraries_npy.py
+++ create_frag_library/mutate-AC-libraries_npy.py
@@ -12,13 +12,13 @@ print((na, pattern), file=sys.stderr)
 dirname = os.path.dirname(os.path.abspath(__file__))
 
 def execute(command):
-    print(command)
+    print("%s\n"%command)
     check_call(command, shell = True)
 
 x = "R"
 dict_mutations = {'A':'G', 'C':'U'}
 if na == 'dna':
-    dict_mutations['C'] = ['T']
+    dict_mutations['C'] = 'T'
     x = "D"
 
 s = ['A','C']
@@ -36,12 +36,15 @@ diffatoms={
  'T':["O4", "C7"]
  }
 
+tmpdir = "/tmp/%s"%na
+os.system('mkdir -p %s/'%tmpdir)
+
 for sequence in sequences:
     seq = [ s for s in sequence ]
     print(sequence)
-    os.system('mkdir -p /tmp/%s'%sequence)
-    execute('%s/npy2pdb.py %s-%s-aa.npy templates/%s.pdb > /tmp/%s.pdb' \
-             %(dirname, sequence, pattern, sequence, sequence))
+    os.system('mkdir -p %s/%s/'%(tmpdir, sequence))
+    execute('%s/npy2pdb.py %s-%s-aa.npy templates/%s.pdb > %s/%s.pdb' \
+             %(dirname, sequence, pattern, sequence, tmpdir, sequence))
     for mut in mutations:
         mutant = ""
         for s, m in zip(seq ,mut):
@@ -51,19 +54,19 @@ for sequence in sequences:
                 c = s
             mutant = mutant + c
         name = "%s-%s"%(mutant, pattern)
-        os.system('mkdir /tmp/%s; mkdir %s'%(mutant, mutant))
+        os.system('mkdir -p %s/%s; mkdir -p %s'%(tmpdir, mutant, mutant))
         #mutate
-        execute('cp /tmp/%s.pdb /tmp/%s.pdb'%(sequence, mutant))
+        execute('cp %s/%s.pdb %s/%s.pdb'%(tmpdir, sequence, tmpdir, mutant))
         for i in range(3):
-            execute("sed -i 's/%s%s     %i/%s%s     %i/' /tmp/%s.pdb" \
-                    %(x, seq[i], i+1, x, mutant[i], i+1, mutant))
-        execute('%s/splitmodel.py /tmp/%s.pdb /tmp/%s/conf-aa > /tmp/%s-aa.list' \
-                 %(dirname, mutant, mutant, name))
-        execute("sed 's/\/tmp\///' /tmp/%s-aa.list > %s-aa.list" \
-                %(name,  name))
-        execute('%s/pdbcompletion.py /tmp/%s-aa.list %s-aa.list \
+            execute("sed -i 's/%s%s     %i/%s%s     %i/' %s/%s.pdb" \
+                    %(x, seq[i], i+1, x, mutant[i], i+1, tmpdir, mutant))
+        execute('%s/splitmodel.py %s/%s.pdb %s/%s/conf-aa > %s/%s-aa.list' \
+                 %(dirname, tmpdir, mutant, tmpdir, mutant, tmpdir, name))
+        execute("sed 's/\/tmp\/%s\///' %s/%s-aa.list > %s-aa.list" \
+                %(na, tmpdir,  name, name))
+        execute('%s/pdbcompletion.py %s/%s-aa.list %s-aa.list \
                 --batch --heavy --%s --patch None 1 None > /dev/null 2> /dev/null' \
-                %(dirname, name, name, na))
+                %(dirname, tmpdir, name, name, na))
         #reduce
         execute("sed 's/\-aa/r/' %s-aa.list > %sr.list "\
                 %(name, name))
@@ -72,4 +75,5 @@ for sequence in sequences:
         execute('%s/reduce-npy.py %s-aa.npy templates/%s.pdb %sr.npy' \
                  %(dirname, mutant, mutant, mutant))
         #cleanup
-        execute("rm -rf /tmp/%s*"%mutant)
+        execute("rm -rf %s/%s*"%(tmpdir, mutant))
+    execute("rm -rf %s/%s*"%(tmpdir, sequence))
