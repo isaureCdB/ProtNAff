9fb6ae36122f39db4c85851817d9535e627b7234
Sjoerd de Vries <sjdv1982@gmail.com>
2019-07-25 22:40:26 +0200

    added tools to add PFAM, UniProt, redundancy masks; updated schema; fragments Numpy file and schema are now generated in search_frag_library

--- search_frag_library.sh
+++ search_frag_library.sh
@@ -9,4 +9,7 @@ SCRIPTS="$wd/$d/search_frag_library/"
 set -u -e
 
 python3 $SCRIPTS/json2npy.py fragments_clust.json fragments_clust.npy
+python3 $SCRIPTS/add_pfam_uniprot.py
+python3 $SCRIPTS/add_seqclust.py
 python3 $SCRIPTS/make_chainschema.py
+python3 -u $SCRIPTS/build-redundancy-masks.py | tee build-redundancy-masks.stats
