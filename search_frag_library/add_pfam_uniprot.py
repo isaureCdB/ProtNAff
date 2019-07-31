8d0853bc42cbad349b62776ed83eaaa3482ce0a2
CHAUVOT DE BEAUCHENE Isaure <isaure.chauvot-de-beauchene@loria.fr>
2019-07-31 19:09:57 +0200

    automatic downloads

--- search_frag_library/add_pfam_uniprot.py
+++ search_frag_library/add_pfam_uniprot.py
@@ -6,19 +6,19 @@ Requires the existence of /tmp/pdb_chain_pfam.tsv.gz
 import json, os
 import pandas as pd
 
-filename = "/tmp/pdb_chain_pfam.tsv.gz"
+filename = "/tmp/pdb_chain_pfam.tsv"
 if not os.path.exists(filename):
     msg = "Requires the existence of /tmp/pdb_chain_pfam.tsv.gz, downloadable from ftp://ftp.ebi.ac.uk/pub/databases/msd/sifts/flatfiles/tsv"
     raise Exception(msg)
 pdb_chain_pfam = pd.read_csv(filename, dtype=str, skiprows=1, delimiter="\t")
 
-mapping = {}                                                                                    
+mapping = {}
 
 for pdb, chain, uniprot, pfam in zip(
-  pdb_chain_pfam["PDB"], pdb_chain_pfam["CHAIN"], 
+  pdb_chain_pfam["PDB"], pdb_chain_pfam["CHAIN"],
   pdb_chain_pfam["SP_PRIMARY"], pdb_chain_pfam["PFAM_ID"]
-): 
-    mapping[pdb.upper(), chain] = uniprot, pfam 
+):
+    mapping[pdb.upper(), chain] = uniprot, pfam
 
 with open("structures.json") as f:
     structures = json.load(f)
@@ -32,9 +32,9 @@ for pdb, struc in structures.items():
             continue
         uniprot[chain] = key[0]
         pfam[chain] = key[1]
-        
+
     struc["pfam"] = pfam
     struc["uniprot"] = uniprot
-    
+
 with open("structures.json", "w") as f:
     json.dump(structures, f, indent=2, sort_keys=True)
