a2f478cdb1dca4e78b531c5e11edcdce077b0c9b
amoniot <antoine.moniot@loria.fr>
2020-02-27 12:10:21 +0100

    Manage problem of mapping on the creation of library

--- create_frag_library/create_libraries.sh
+++ create_frag_library/create_libraries.sh
@@ -8,7 +8,7 @@ d=`dirname "$0"`
 
 echo "create PDB fragments (for mutating) from clust1.0, $m"
 
-$d/select-struct-npy.py $m-aa-fit.npy  $name-aa.npy  --struct `awk '{print $4}' $name`
+#$d/select-struct-npy.py $m-aa-fit.npy  $name-aa.npy  --struct `awk '{print $4}' $name`
 
 $d/npy2pdb.py $name-aa.npy  templates/$m.pdb   >  /tmp/$name-aa.pdb
 
