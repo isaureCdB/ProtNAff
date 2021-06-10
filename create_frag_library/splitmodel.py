91c2605476546974ed130ec5ba11ace1f6ddaa01
antoine_moniot <antoine.moniot@loria.fr>
2021-06-10 16:20:08 +0200

    Translate splitmodel.py from python2 to 3

--- create_frag_library/splitmodel.py
+++ create_frag_library/splitmodel.py
@@ -11,17 +11,17 @@ header = []
 count = 0
 for l in open(f):
   l = l.rstrip("\n")
-  if l.startswith("MODEL"): 
+  if l.startswith("MODEL"):
     if count > 0: ff.close()
     count += 1
     target = template+"-"+str(count)+".pdb"
-    print target
+    print(target)
     ff = open(target, "w")
-    for h in header: print >> ff, h
+    for h in header: ff.write(h + "\n")
     continue
   if l.startswith("ENDMDL"): continue
   if count == 0: header.append(l)
-  else: print >> ff, l
+  else: ff.write(l + "\n")
 
 if size is not None and count != size:
   raise Exception("ERROR in splitmodel: model count should be %d, is %d" % (size, count))
