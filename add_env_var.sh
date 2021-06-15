cb48a7bb31f4222384fba401221cc74542737310
antoine_moniot <antoine.moniot@loria.fr>
2021-06-15 08:40:44 +0200

    Add the environment variable with a bash script

---
+++ add_env_var.sh
@@ -0,0 +1,4 @@
+echo 'export MY_CONDA=~/miniconda3' >> ~/.bashrc
+echo 'export PROTNAFF=$PWD' >> ~/.bashrc
+echo 'export PATH=$PROTNAFF:$PATH' >> ~/.bashrc
+echo 'export PYTHONPATH=$PROTNAFF:$PYTHONPATH' >> ~/.bashrc
